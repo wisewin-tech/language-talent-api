5a6c83278dbdc1972a0568e6be6a99ba38d7edd5
18631323023 <qq492690314>
2019-05-06 15:40:26 +0800

    实体类

---
+++ src/main/java/com/wisewin/api/entity/bo/UserScoreRecordBO.java
@@ -0,0 +1,98 @@
+package com.wisewin.api.entity.bo;
+
+import com.wisewin.api.common.base.BaseModel;
+
+import java.util.Date;
+
+public class UserScoreRecordBO extends BaseModel{
+    private Integer id; //用户分数记录表
+    private Integer userId; //用户id
+    private Integer chapterId; //课时id
+    private Integer score; //分数
+    private Date doExerciseTime; //做题时间
+    private String writeAnswer; //用户做题答案
+    private Integer createUserId; //创建人id
+    private Date createTime; //创建时间
+    private Integer updateUserId; //修改人id
+    private Date updateTime; //修改时间
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public Integer getChapterId() {
+        return chapterId;
+    }
+
+    public void setChapterId(Integer chapterId) {
+        this.chapterId = chapterId;
+    }
+
+    public Integer getScore() {
+        return score;
+    }
+
+    public void setScore(Integer score) {
+        this.score = score;
+    }
+
+    public Date getDoExerciseTime() {
+        return doExerciseTime;
+    }
+
+    public void setDoExerciseTime(Date doExerciseTime) {
+        this.doExerciseTime = doExerciseTime;
+    }
+
+    public String getWriteAnswer() {
+        return writeAnswer;
+    }
+
+    public void setWriteAnswer(String writeAnswer) {
+        this.writeAnswer = writeAnswer;
+    }
+
+    public Integer getCreateUserId() {
+        return createUserId;
+    }
+
+    public void setCreateUserId(Integer createUserId) {
+        this.createUserId = createUserId;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Integer getUpdateUserId() {
+        return updateUserId;
+    }
+
+    public void setUpdateUserId(Integer updateUserId) {
+        this.updateUserId = updateUserId;
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+}
---
+++ src/main/java/com/wisewin/api/entity/dto/UserStudyDetailsDTO.java
@@ -0,0 +1,78 @@
+package com.wisewin.api.entity.dto;
+
+import java.util.Date;
+
+public class UserStudyDetailsDTO {
+    private Integer id; //学习详情表
+    private Integer userId; //用户id
+    private Date studyDate; //学习日期
+    private Integer studyDuration; //学习时长(分钟)
+    private Integer createUserId; //创建人id
+    private Date createTime; //创建时间
+    private Integer updateUserId; //修改人id
+    private Date updateTime; //修改时间
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public Date getStudyDate() {
+        return studyDate;
+    }
+
+    public void setStudyDate(Date studyDate) {
+        this.studyDate = studyDate;
+    }
+
+    public Integer getStudyDuration() {
+        return studyDuration;
+    }
+
+    public void setStudyDuration(Integer studyDuration) {
+        this.studyDuration = studyDuration;
+    }
+
+    public Integer getCreateUserId() {
+        return createUserId;
+    }
+
+    public void setCreateUserId(Integer createUserId) {
+        this.createUserId = createUserId;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Integer getUpdateUserId() {
+        return updateUserId;
+    }
+
+    public void setUpdateUserId(Integer updateUserId) {
+        this.updateUserId = updateUserId;
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+}
