641154d5c55717a2fa384eb0c38a485dab757d13
dingxinyang <475341496@qq.com>
2019-05-17 15:20:22 +0800

    dxy

---
+++ src/main/webapp/js/activity.js
@@ -0,0 +1,44 @@
+$(function () {
+    var id = getUrl('id');
+    if(id){
+        getDataInfo(id);
+    }
+});
+
+function getDataInfo(id) {
+    $.ajax({
+        url: '/Discover/queryDiscoveractivity',
+        type: 'POST',
+        dataType: 'json',
+        data: {id: id},
+        success: function (res) {
+            for(var item in res.data){
+                if(item=="phone"){
+                    $('#'+item).text(res.data[item])
+                    $('#'+item).attr('href', "tel:"+res.data[item]);
+                }else if(item=="thumbnail"){
+                    $('#'+item).attr('src', res.data[item]);
+                }else {
+                    $('#'+item).html(res.data[item])
+                }
+
+            }
+        }
+    })
+}
+
+function getUrl(name) {
+    var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
+    var r = window.location.search.substr(1).match(reg);
+    if (name) {
+        if (r != null)
+            return decodeURI(r[2]);
+        return null;
+    } else {
+        r = window.location.search.substr(1);
+        if (r != null)
+            return decodeURI(r);
+        return null;
+    }
+}
+
---
+++ src/main/webapp/js/discover.js
@@ -0,0 +1,37 @@
+$(function () {
+    var id = getUrl('id');
+    if(id){
+        getDataInfo(id);
+    }
+})
+
+function getDataInfo(id) {
+    $.ajax({
+        url: '/Discover/queryDiscoveractivity',
+        type: 'POST',
+        data: {id: id},
+        dataType: 'json',
+        success: function (res) {
+            $('title').text(res.data.title);
+            for(var item in res.data){
+                $('#'+item).html(res.data[item]);
+            }
+            $(".ql-video").contents().find("video").removeAttr('controls');
+        }
+    })
+}
+
+function getUrl(name) {
+    var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
+    var r = window.location.search.substr(1).match(reg);
+    if (name) {
+        if (r != null)
+            return decodeURI(r[2]);
+        return null;
+    } else {
+        r = window.location.search.substr(1);
+        if (r != null)
+            return decodeURI(r);
+        return null;
+    }
+}
