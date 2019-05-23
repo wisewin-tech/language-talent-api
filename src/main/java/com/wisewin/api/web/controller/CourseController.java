0945bedd6ad1492d950d0e2246bda4c75ae40028
18631323023 <qq492690314>
2019-05-23 15:34:53 +0800

    修复bug

--- src/main/java/com/wisewin/api/web/controller/CourseController.java
+++ src/main/java/com/wisewin/api/web/controller/CourseController.java
@@ -44,10 +44,13 @@ public class CourseController extends BaseCotroller {
         CourseDetailsResultBO courseDetailsResultBO = courseService.courseDetailsCourse(id);
         List<CourseDetailsLevelResultBO> levelBOS= courseService.courseDetailsLevel(id);
         String status = orderService.getStatusByCourseId(userId,id);
-        if ("yes".equals(status)){
-            courseDetailsResultBO.setBuyOrNot("yes");
-        }else {
-            courseDetailsResultBO.setBuyOrNot("no");
+        if (courseDetailsResultBO!=null) {
+            if ("yes".equals(status)) {
+                courseDetailsResultBO.setBuyOrNot("yes");
+            }else {
+                courseDetailsResultBO.setBuyOrNot("no");
+            }
+
         }
         Map map = new HashMap();
         map.put("courseDetailsResultBO",courseDetailsResultBO);
