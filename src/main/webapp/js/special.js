b1c0a048a8aff79bcadb18f12cdcf90c9f2cb55a
dingxinyang <475341496@qq.com>
2019-05-29 10:43:54 +0800

    dxy

---
+++ src/main/webapp/js/special.js
@@ -0,0 +1,37 @@
+$(function () {
+    var id = getUrl('id');
+    if(id){
+        getDataInfo(id);
+    }
+})
+
+function getDataInfo(id) {
+    $.ajax({
+        url: '/Special/selectSpecialBOById',
+        type: 'POST',
+        data: {specialId: id},
+        dataType: 'json',
+        success: function (res) {
+            $('title').text(res.data.specialBO.title);
+            for(var item in res.data.specialBO){
+                $('#'+item).html(res.data.specialBO[item]);
+            }
+            $(".ql-video").contents().find("video").removeAttr('controls');
+        }
+    })
+}
+
+function getUrl(name) {
+    var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
+    var r = window.location.search.substr(1).match(reg);
+    if (name) {
+        if (r != null)
+            return decodeURI(r[2]);
+        return null;
+    } else {
+        r = window.location.search.substr(1);
+        if (r != null)
+            return decodeURI(r);
+        return null;
+    }
+}
