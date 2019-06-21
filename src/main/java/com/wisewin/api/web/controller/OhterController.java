969e3e9bd57d49b59d92597672d3eaf84320afe1
xiaowen1024 <15214440324@163.com>
2019-06-21 16:08:50 +0800

    bug修复

--- src/main/java/com/wisewin/api/web/controller/OhterController.java
+++ src/main/java/com/wisewin/api/web/controller/OhterController.java
@@ -1,8 +1,12 @@
 package com.wisewin.api.web.controller;
 
 import com.wisewin.api.entity.bo.ClauseBO;
+import com.wisewin.api.entity.bo.CourseDetailsResultBO;
+import com.wisewin.api.entity.bo.LanguageDetailsResultBO;
 import com.wisewin.api.entity.dto.ResultDTOBuilder;
 import com.wisewin.api.service.ClauseService;
+import com.wisewin.api.service.CourseService;
+import com.wisewin.api.service.LanguageService;
 import com.wisewin.api.util.JsonUtils;
 import com.wisewin.api.util.StringUtils;
 import com.wisewin.api.web.controller.base.BaseCotroller;
@@ -20,9 +24,20 @@ import javax.servlet.http.HttpServletResponse;
 public class OhterController extends BaseCotroller {
     @Resource
     private ClauseService  clauseService;
+    @Resource
+    private LanguageService languageService;
+    @Resource
+    private CourseService courseService;
+
     static final Logger log = LoggerFactory.getLogger(OhterController.class);
 
 
+    /**
+     * 查询购买须知类的东西
+     * @param request
+     * @param response
+     * @param config
+     */
     @RequestMapping("/queryConfig")
     public void queryConfig(HttpServletRequest request,HttpServletResponse response,String config) {
         if(StringUtils.isEmpty(config)){
@@ -38,9 +53,42 @@ public class OhterController extends BaseCotroller {
     }
 
 
+    /**
+     * 查询课程亮点 ||  语言亮点
+     * @param request
+     * @param response
+     * @param type course  || language
+     * @param id
+     */
+    @RequestMapping("/queryLightspot")
+    public void queryLightspot(HttpServletRequest request,HttpServletResponse response,String type,Integer id) {
+        if(StringUtils.isEmpty(type) || id==null){
+            String json = JsonUtils.getJsonString4JavaPOJO(ResultDTOBuilder.failure("0000001"));
+            super.safeJsonPrint(response, json);
+            return;
+        }
+
+        String msg="";
+        //查询课程的
+        if("course".equals(type)){
+            CourseDetailsResultBO courseDetailsResultBO = courseService.courseDetailsCourse(id);
+            if(courseDetailsResultBO!=null){
+                msg= courseDetailsResultBO.getCourseLightspot();
+            }
+        }
+        //查询语言的
+        if("language".equals(type)){
+            LanguageDetailsResultBO language = languageService.languageDetails(id);
+            if(language!=null){
+                msg= language.getLanguageLightspot();
+            }
+        }
 
 
-
+        String json = JsonUtils.getJsonString4JavaPOJO(ResultDTOBuilder.success(msg));
+        super.safeJsonPrint(response, json);
+        return;
+    }
 
 
 }
