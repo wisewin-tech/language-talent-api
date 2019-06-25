5f866c7b900d6b528638d0eb7cb7f7986f6df0ff
hp <475341496@qq.com>
2019-06-25 16:47:51 +0800

    dxy

--- src/main/webapp/js/activity.js
+++ src/main/webapp/js/activity.js
@@ -22,10 +22,28 @@ function getDataInfo(id) {
                 }else {
                     $('#'+item).html(res.data[item])
                 }
-
             }
+            $('#week').test(FormatWeek(res.data.activitytime))
         }
     })
+}
+function FormatWeek(val){
+    switch (new Date(val.time).getDay()){
+        case 1:
+            return "星期一";
+        case 2:
+            return "星期二";
+        case 3:
+            return "星期三";
+        case 4:
+            return "星期四";
+        case 5:
+            return "星期五";
+        case 6:
+            return "星期六";
+        case 7:
+            return "星期七";
+    }
 }
 
 function getUrl(name) {
